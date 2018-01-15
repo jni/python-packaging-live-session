ebe77a99f460023255788b1d4b8a84d71391ceb6
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2018-01-16 10:18:14 +1100

    Change module to package

--- setup.py
+++ setup.py
@@ -107,9 +107,9 @@ setup(
     # the `py_modules` argument instead as follows, which will expect a file
     # called `my_module.py` to exist:
     #
-    py_modules=["foo"],
+    # py_modules=["foo"],
     #
-    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
 
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
